af974091a7a27ab0c18e937a1bc443e8e31ff31f
Jordan Meier and Ronando Long <jbirdmeier@gmail.com>
2016-02-10 11:45:22 -0800

    add a Place class, constructor, getter, setter, save method and getAll method

---
+++ src/Place.php
@@ -0,0 +1,31 @@
+<?php
+class Place
+{
+    private $city;
+
+    function __construct($place_city);
+    $this->city = $place_city;
+
+    function setCity($new_city)
+    {
+        $this->city = (string) $new_city;
+    }
+
+    function getCity()
+    {
+        return $this->city;
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_places'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_places'];
+    }
+
+}
+
+?>
